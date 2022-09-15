7c8156bc0cea2a5d020bd66d84ed30746448c00d
ChosenOne959 <87606392+ChosenOne959@users.noreply.github.com>
2022-09-15 12:56:02 +0800

    Delete airsim_python directory

--- airsim_python/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-center = np.array([[0],[5]])
-pos_reserve = np.array([[0.],[0.],[-3.]])
-pos = [[6],[13],[10]]
-dp = pos[0:2]-center
-print(dp)
-print(pos_reserve)
-print("norm:",np.linalg.norm(dp))
