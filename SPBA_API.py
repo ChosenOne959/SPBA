aca97d1c9dfa2d0ef8ae7688dd2d1a37ad9412b3
riacd <1927208797@qq.com>
2022-10-12 16:12:14 +0800

    add flight_control frame

--- SPBA_API.py
+++ SPBA_API.py
@@ -6,6 +6,7 @@ import math
 import time
 from scipy import linalg
 import json
+import threading
 
 nan = float("nan")
 
@@ -423,142 +424,207 @@ class AirSimSettings:
         }
         return unreal_engine
 
+class GroundTruthEstimation(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.client = airsim.MultirotorClient()
+        self.dt = 1
+        self.ImuData = {}
+        self.BarometerData = {}
+        self.MagnetometerData = {}
+        self.GpsData = {}
+
+    def run(self):    #把要执行的代码写到run函数里面 调用start创建线程来运行run函数
+        print("Starting GroundTruthEstimation")
+        while True:
+            self.update_sensor_data()
+            print("Sensor Updated!")
+            self.update_estimation()
+            time.sleep(self.dt)
+
+    def update_sensor_data(self):
+        self.ImuData = self.client.getImuData()
+        self.BarometerData = self.client.getBarometerData()
+        self.MagnetometerData = self.client.getMagnetometerData()
+        self.GpsData = self.client.getGpsData()
+
+    def update_estimation(self):
+        pass
+
+    def get_estimation(self):
+        pass
+
+    def log(self):
+        pass
 
 
+class Control:
+    def __init__(self):
+        self.GroundTruth = GroundTruthEstimation()
+        # self.GroundTruth.start()
 
+        self.client = airsim.MultirotorClient()
+        self.client.enableApiControl(True)  # 获取控制权
+        self.client.armDisarm(True)  # 解锁
 
+    # some of the APIs here are actually airsim APIs defined in client.py
 
+    # low level
+    def moveByMotorPWMsAsync(self, front_right_pwm, rear_left_pwm, front_left_pwm, rear_right_pwm, duration, vehicle_name =''):
+        return self.client.moveByMotorPWMsAsync(front_right_pwm, rear_left_pwm, front_left_pwm, rear_right_pwm, duration, vehicle_name)
 
-def test_traj_creat():
-    p_traj = np.zeros([2, 1600])
-    v_traj = np.zeros([2, 1600])
-    a_traj = np.zeros([2, 1600])
+    def moveByRollPitchYawZAsync(self, roll, pitch, yaw, z, duration, vehicle_name=''):
+        self.client.moveByRollPitchYawZAsync(roll, pitch, yaw, z, duration, vehicle_name)
 
-    for i in range(400):
-        theta = math.pi - math.pi / 400 * i
-        p_traj[0, i] = 5 * math.cos(theta) + 5
-        p_traj[1, i] = 5 * math.sin(theta)
-        v_traj[0, i] = 1.9635 * math.cos(theta - math.pi / 2)
-        v_traj[1, i] = 1.9635 * math.sin(theta - math.pi / 2)
-        a_traj[0, i] = -0.7712 * math.cos(theta)
-        a_traj[1, i] = -0.7712 * math.sin(theta)
-    for i in range(400):
-        theta = math.pi + math.pi / 400 * i
-        p_traj[0, i + 400] = 5 * math.cos(theta) + 15
-        p_traj[1, i + 400] = 5 * math.sin(theta)
-        v_traj[0, i + 400] = 1.9635 * math.cos(theta + math.pi / 2)
-        v_traj[1, i + 400] = 1.9635 * math.sin(theta + math.pi / 2)
-        a_traj[0, i + 400] = -0.7712 * math.cos(theta)
-        a_traj[1, i + 400] = -0.7712 * math.sin(theta)
-    for i in range(400):
-        theta = math.pi / 400 * i
-        p_traj[0, i + 800] = 5 * math.cos(theta) + 15
-        p_traj[1, i + 800] = 5 * math.sin(theta)
-        v_traj[0, i + 800] = 1.9635 * math.cos(theta + math.pi / 2)
-        v_traj[1, i + 800] = 1.9635 * math.sin(theta + math.pi / 2)
-        a_traj[0, i + 800] = -0.7712 * math.cos(theta)
-        a_traj[1, i + 800] = -0.7712 * math.sin(theta)
-    for i in range(400):
-        theta = 2 * math.pi - math.pi / 400 * i
-        p_traj[0, i + 1200] = 5 * math.cos(theta) + 5
-        p_traj[1, i + 1200] = 5 * math.sin(theta)
-        v_traj[0, i + 1200] = 1.9635 * math.cos(theta - math.pi / 2)
-        v_traj[1, i + 1200] = 1.9635 * math.sin(theta - math.pi / 2)
-        a_traj[0, i + 1200] = -0.7712 * math.cos(theta)
-        a_traj[1, i + 1200] = -0.7712 * math.sin(theta)
-    return p_traj, v_traj, a_traj
+    # tracking
+    def moveOnPathAsync(self, path, velocity, lookahead=-1, adaptive_lookahead=1, vehicle_name=''):
+        timeout_sec = 3e+38
+        drivetrain = DrivetrainType.MaxDegreeOfFreedom
+        yaw_mode = YawMode()
+        return self.client.moveOnPathAsync(path, velocity, timeout_sec, drivetrain, yaw_mode,
+        lookahead, adaptive_lookahead, vehicle_name)
 
+    def moveOnPath_LQR(self, p_traj, v_traj, a_traj):
+        # 解算离散LQR的反馈矩阵
 
-def plot(client, p_traj, color_rgba=None, is_persistent=True):
-    if color_rgba is None:
-        color_rgba = [0.0, 1.0, 0.0, 1.0]
-    plot_traj = [airsim.Vector3r(p_traj[0, 0], p_traj[1, 0], -5)]
+        def DLQR(A, B, Q, R):
+            S = np.matrix(linalg.solve_discrete_are(A, B, Q, R))
+            K = np.matrix(linalg.inv(B.T * S * B + R) * (B.T * S * A))
+            return K
 
-    for i in range(1600):
-        plot_traj += [airsim.Vector3r(p_traj[0, i], p_traj[1, i], -5)]
-    client.simPlotLineList(plot_traj, color_rgba=color_rgba, is_persistent=is_persistent)
-
-
-def moveOnPath_LQR(client, p_traj, v_traj, a_traj):
-    # 解算离散LQR的反馈矩阵
-
-    def DLQR(A, B, Q, R):
-        S = np.matrix(linalg.solve_discrete_are(A, B, Q, R))
-        K = np.matrix(linalg.inv(B.T * S * B + R) * (B.T * S * A))
-        return K
-
-    # 四旋翼加速度控制
-    def move_by_acceleration_horizontal(client, ax_cmd, ay_cmd, z_cmd, duration=1):
-        # 读取自身yaw角度
-        state = client.simGetGroundTruthKinematics()
-        angles = airsim.to_eularian_angles(state.orientation)
-        yaw_my = angles[2]
-        g = 9.8  # 重力加速度
-        sin_yaw = math.sin(yaw_my)
-        cos_yaw = math.cos(yaw_my)
-        A_psi = np.array([[sin_yaw, cos_yaw], [-cos_yaw, sin_yaw]])
-        A_psi_inverse = np.linalg.inv(A_psi)
-        angle_h_cmd = 1 / g * np.dot(A_psi_inverse, np.array([[-ax_cmd], [-ay_cmd]]))
-        a_x_cmd = math.atan(angle_h_cmd[0, 0])
-        a_y_cmd = -math.atan(angle_h_cmd[1, 0])
-        client.moveByRollPitchYawZAsync(a_x_cmd, a_y_cmd, 0, z_cmd, duration)
-        return
-
-    # configuration
-    dt = 0.02
-    A = np.array([[1, 0, dt, 0],
-                  [0, 1, 0, dt],
-                  [0, 0, 1, 0],
-                  [0, 0, 0, 1]])
-    B = np.array([[0, 0],
-                  [0, 0],
-                  [dt, 0],
-                  [0, dt]])
-    Q = np.diag([2, 2, 2, 2])
-    R = np.diag([.1, .1])
-    K = DLQR(A, B, Q, R)
-
-    plot_last_pos = [airsim.Vector3r(0, 0, -5)]
-
-    for t in range(1600):
-        # 读取当前的位置和速度
-        UAV_state = client.simGetGroundTruthKinematics()
-        pos_now = np.array([[UAV_state.position.x_val], [UAV_state.position.y_val], [UAV_state.position.z_val]])
-        vel_now = np.array(
-            [[UAV_state.linear_velocity.x_val], [UAV_state.linear_velocity.y_val], [UAV_state.linear_velocity.z_val]])
-        state_now = np.vstack((pos_now[0:2], vel_now[0:2]))
-        # 目标状态
-        state_des = np.vstack((p_traj[:, t:t + 1], v_traj[:, t:t + 1]))
-        # LQR轨迹跟踪
-        a = -np.dot(K, state_now - state_des) + a_traj[:, t:t + 1]
         # 四旋翼加速度控制
-        move_by_acceleration_horizontal(client, a[0, 0], a[1, 0], -5)
-        # 画图
-        plot_v_start = [airsim.Vector3r(pos_now[0, 0], pos_now[1, 0], pos_now[2, 0])]
-        plot_v_end = pos_now + vel_now
-        plot_v_end = [airsim.Vector3r(plot_v_end[0, 0], plot_v_end[1, 0], plot_v_end[2, 0])]
-        client.simPlotArrows(plot_v_start, plot_v_end, arrow_size=8.0, color_rgba=[0.0, 0.0, 1.0, 1.0])
-        client.simPlotLineList(plot_last_pos + plot_v_start, color_rgba=[1.0, 0.0, 0.0, 1.0], is_persistent=True)
-        plot_last_pos = plot_v_start
-        time.sleep(dt)
+        def move_by_acceleration_horizontal(client, ax_cmd, ay_cmd, z_cmd, duration=1):
+            # 读取自身yaw角度
+            state = client.simGetGroundTruthKinematics()
+            angles = airsim.to_eularian_angles(state.orientation)
+            yaw_my = angles[2]
+            g = 9.8  # 重力加速度
+            sin_yaw = math.sin(yaw_my)
+            cos_yaw = math.cos(yaw_my)
+            A_psi = np.array([[sin_yaw, cos_yaw], [-cos_yaw, sin_yaw]])
+            A_psi_inverse = np.linalg.inv(A_psi)
+            angle_h_cmd = 1 / g * np.dot(A_psi_inverse, np.array([[-ax_cmd], [-ay_cmd]]))
+            a_x_cmd = math.atan(angle_h_cmd[0, 0])
+            a_y_cmd = -math.atan(angle_h_cmd[1, 0])
+            self.moveByRollPitchYawZAsync(a_x_cmd, a_y_cmd, 0, z_cmd, duration)
+            return
 
+        # configuration
+        dt = 0.02
+        A = np.array([[1, 0, dt, 0],
+                      [0, 1, 0, dt],
+                      [0, 0, 1, 0],
+                      [0, 0, 0, 1]])
+        B = np.array([[0, 0],
+                      [0, 0],
+                      [dt, 0],
+                      [0, dt]])
+        Q = np.diag([2, 2, 2, 2])
+        R = np.diag([.1, .1])
+        K = DLQR(A, B, Q, R)
 
-def LQR_fly_test():
-    p_traj, v_traj, a_traj = test_traj_creat()
+        plot_last_pos = [airsim.Vector3r(0, 0, -5)]
 
-    client = airsim.MultirotorClient()  # connect to the AirSim simulator
-    client.enableApiControl(True)  # 获取控制权
-    client.armDisarm(True)  # 解锁
-    client.takeoffAsync().join()  # 起飞
-    client.moveToZAsync(-5, 1).join()  # 上升到5米高度
+        for t in range(1600):
+            # 读取当前的位置和速度
+            UAV_state = self.client.simGetGroundTruthKinematics()
+            pos_now = np.array([[UAV_state.position.x_val], [UAV_state.position.y_val], [UAV_state.position.z_val]])
+            vel_now = np.array(
+                [[UAV_state.linear_velocity.x_val], [UAV_state.linear_velocity.y_val],
+                 [UAV_state.linear_velocity.z_val]])
+            state_now = np.vstack((pos_now[0:2], vel_now[0:2]))
+            # 目标状态
+            state_des = np.vstack((p_traj[:, t:t + 1], v_traj[:, t:t + 1]))
+            # LQR轨迹跟踪
+            a = -np.dot(K, state_now - state_des) + a_traj[:, t:t + 1]
+            # 四旋翼加速度控制
+            move_by_acceleration_horizontal(self.client, a[0, 0], a[1, 0], -5)
+            # 画图
+            plot_v_start = [airsim.Vector3r(pos_now[0, 0], pos_now[1, 0], pos_now[2, 0])]
+            plot_v_end = pos_now + vel_now
+            plot_v_end = [airsim.Vector3r(plot_v_end[0, 0], plot_v_end[1, 0], plot_v_end[2, 0])]
+            self.client.simPlotArrows(plot_v_start, plot_v_end, arrow_size=8.0, color_rgba=[0.0, 0.0, 1.0, 1.0])
+            self.client.simPlotLineList(plot_last_pos + plot_v_start, color_rgba=[1.0, 0.0, 0.0, 1.0], is_persistent=True)
+            plot_last_pos = plot_v_start
+            time.sleep(dt)
 
-    plot(client, p_traj)  # 画出规划路径
+    def custom_tracking(self, **kwargs):
+        pass
 
-    moveOnPath_LQR(client, p_traj, v_traj, a_traj)
+class Multirotor:
+    def __init__(self):
+        self.client = airsim.MultirotorClient()
+        self.flight_control = Control()
 
-    client.landAsync().join()
-    client.armDisarm(False)  # 上锁
-    client.enableApiControl(False)  # 释放控制权
+    # create trajectory
+    @staticmethod
+    def LQR_8_traj():
+        p_traj = np.zeros([2, 1600])
+        v_traj = np.zeros([2, 1600])
+        a_traj = np.zeros([2, 1600])
+
+        for i in range(400):
+            theta = math.pi - math.pi / 400 * i
+            p_traj[0, i] = 5 * math.cos(theta) + 5
+            p_traj[1, i] = 5 * math.sin(theta)
+            v_traj[0, i] = 1.9635 * math.cos(theta - math.pi / 2)
+            v_traj[1, i] = 1.9635 * math.sin(theta - math.pi / 2)
+            a_traj[0, i] = -0.7712 * math.cos(theta)
+            a_traj[1, i] = -0.7712 * math.sin(theta)
+        for i in range(400):
+            theta = math.pi + math.pi / 400 * i
+            p_traj[0, i + 400] = 5 * math.cos(theta) + 15
+            p_traj[1, i + 400] = 5 * math.sin(theta)
+            v_traj[0, i + 400] = 1.9635 * math.cos(theta + math.pi / 2)
+            v_traj[1, i + 400] = 1.9635 * math.sin(theta + math.pi / 2)
+            a_traj[0, i + 400] = -0.7712 * math.cos(theta)
+            a_traj[1, i + 400] = -0.7712 * math.sin(theta)
+        for i in range(400):
+            theta = math.pi / 400 * i
+            p_traj[0, i + 800] = 5 * math.cos(theta) + 15
+            p_traj[1, i + 800] = 5 * math.sin(theta)
+            v_traj[0, i + 800] = 1.9635 * math.cos(theta + math.pi / 2)
+            v_traj[1, i + 800] = 1.9635 * math.sin(theta + math.pi / 2)
+            a_traj[0, i + 800] = -0.7712 * math.cos(theta)
+            a_traj[1, i + 800] = -0.7712 * math.sin(theta)
+        for i in range(400):
+            theta = 2 * math.pi - math.pi / 400 * i
+            p_traj[0, i + 1200] = 5 * math.cos(theta) + 5
+            p_traj[1, i + 1200] = 5 * math.sin(theta)
+            v_traj[0, i + 1200] = 1.9635 * math.cos(theta - math.pi / 2)
+            v_traj[1, i + 1200] = 1.9635 * math.sin(theta - math.pi / 2)
+            a_traj[0, i + 1200] = -0.7712 * math.cos(theta)
+            a_traj[1, i + 1200] = -0.7712 * math.sin(theta)
+        return p_traj, v_traj, a_traj
+
+    @staticmethod
+    def custom_traj():
+        pass
+
+    # others
+    def plot(self, path, color_rgba=None, is_persistent=True):
+        if color_rgba is None:
+            color_rgba = [0.0, 1.0, 0.0, 1.0]
+        plot_traj = [airsim.Vector3r(path[0, 0], path[1, 0], -5)]
+
+        for i in range(1600):
+            plot_traj += [airsim.Vector3r(path[0, i], path[1, i], -5)]
+        self.client.simPlotLineList(plot_traj, color_rgba=color_rgba, is_persistent=is_persistent)
+
+    # test
+    def LQR_fly_test(self):
+        self.client.takeoffAsync().join()  # 起飞
+        self.client.moveToZAsync(-5, 1).join()  # 上升到5米高度
+
+        path = self.LQR_8_traj()
+        self.plot(path[0])  # 画出规划路径
+
+        self.flight_control.moveOnPath_LQR(path[0], path[1], path[2])
+
+        self.client.landAsync().join()
+        self.client.armDisarm(False)  # 上锁
+        self.client.enableApiControl(False)  # 释放控制权
+
 
 def init():
     client = airsim.MultirotorClient()  # connect to the AirSim simulator
@@ -571,4 +637,5 @@ if __name__ == '__main__':
     PATH = 'C:/Users/huyutong2020/Documents/AirSim/settings.json'
     settings = AirSimSettings(PATH)
     settings.reset()
-
+    drone = Multirotor()
+    drone.LQR_fly_test()
